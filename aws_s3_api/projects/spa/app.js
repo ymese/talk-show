b23e52bb0bf69448569a1411a30f17f33afda22e
Thinh Nguyen <thinhnp@ymese.com>
2018-06-09 00:06:09 +0700

    prepare

--- aws_s3_api/projects/spa/app.js
+++ aws_s3_api/projects/spa/app.js
@@ -3,31 +3,10 @@ window.addEventListener("load", function(event) {
 		el: '#app',
 		data: {
 			appTitle: "World Cup 2018 Russia",
-			groups: [{
-				name: "Group A",
-				teams: ["RUSSIA", "SAUDI ARABIA", "EGYPT", "URUGUAY"]
-			},{
-				name: "Group B",
-				teams: ["RUSSIA", "SAUDI ARABIA", "EGYPT", "URUGUAY"]
-			},{
-				name: "Group C",
-				teams: ["RUSSIA", "SAUDI ARABIA", "EGYPT", "URUGUAY"]
-			},{
-				name: "Group D",
-				teams: ["RUSSIA", "SAUDI ARABIA", "EGYPT", "URUGUAY"]
-			},{
-				name: "Group E",
-				teams: ["RUSSIA", "SAUDI ARABIA", "EGYPT", "URUGUAY"]
-			},{
-				name: "Group F",
-				teams: ["RUSSIA", "SAUDI ARABIA", "EGYPT", "URUGUAY"]
-			},{
-				name: "Group G",
-				teams: ["RUSSIA", "SAUDI ARABIA", "EGYPT", "URUGUAY"]
-			},{
-				name: "Group H",
-				teams: ["RUSSIA", "SAUDI ARABIA", "EGYPT", "URUGUAY"]
-			}]
+			groups: []
+		},
+		mounted: function() {
+			
 		}
 	});
 });
